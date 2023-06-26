3c85c305b70926429f84c01cec59f86d7622cd12
Adrian Lübke <aluebke@ai-ag.de>
2023-06-26 16:34:47 +0200

    Added admin template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ConfigProvider } from 'antd';
+import { HelmetProvider } from 'react-helmet-async';
+import deDe from 'antd/lib/locale/de_DE';
+import enUS from 'antd/lib/locale/en_US';
+import GlobalStyle from './styles/GlobalStyle';
+import 'typeface-montserrat';
+import 'typeface-lato';
+import { AppRouter } from './components/router/AppRouter';
+import { useLanguage } from './hooks/useLanguage';
+import { useAutoNightMode } from './hooks/useAutoNightMode';
+import { usePWA } from './hooks/usePWA';
+import { useThemeWatcher } from './hooks/useThemeWatcher';
+import { useAppSelector } from './hooks/reduxHooks';
+import { themeObject } from './styles/themes/themeVariables';
 
-function App() {
+const App: React.FC = () => {
+  const { language } = useLanguage();
+  const theme = useAppSelector((state) => state.theme.theme);
+
+  usePWA();
+
+  useAutoNightMode();
+
+  useThemeWatcher();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <meta name="theme-color" content={themeObject[theme].primary} />
+      <GlobalStyle />
+      <HelmetProvider>
+        <ConfigProvider locale={language === 'en' ? enUS : deDe}>
+          <AppRouter />
+        </ConfigProvider>
+      </HelmetProvider>
+    </>
   );
-}
+};
 
 export default App;
